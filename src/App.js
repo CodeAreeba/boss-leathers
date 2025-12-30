9a0b6b179ec9f3e79ff97422d2812cd160afe2dd
CodeAreeba <areebaabdullah111@gmail.com>
2025-12-30 16:35:34 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
  
 import './App.css';
+<<<<<<< Updated upstream
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from './MainLayout/MainLayout';
 import Home from './Pages/Home/Home';
@@ -10,10 +11,18 @@ import ShippingPolicyPage from './Pages/ShippingPolicy/ShippingPolicyPage';
 import CartPage from './Pages/Cart/CartPage';
 import CheckoutPage from './Pages/Checkout/CheckoutPage';
 import Cart from "./Components/CartItems/CartItems";
+=======
+import Footer from './Footer/Footer';
+import NavBar from './Components/NavBar/NavBar';
+import CardsSection from './Components/CardsSection/CardsSection';
+import Sidebar from './Sidebar/Sidebar';
+import SearchOverlay from './Components/SearchOverlay/SearchOverlay';
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
@@ -30,6 +39,13 @@ function App() {
         </Route>
       </Routes>
     </BrowserRouter>
+=======
+      <NavBar />
+      
+      <CardsSection />
+      <Footer />
+      
+>>>>>>> Stashed changes
        
     </div>
   );
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -6,63 +6,48 @@ import emailIcon from '../../Assets/emailIcon.png';
 import cart from '../../Assets/Cart.png';
 import menu from '../../Assets/menuIcon.png';
 import { FiSearch } from "react-icons/fi";
-import Sidebar from "../Sidebar/Sidebar";
 import SearchOverlay from '../SearchOverlay/SearchOverlay';
-import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
   const [isSearchOpen, setIsSearchOpen] = useState(false);
-  const navigate = useNavigate();
-
-  const toggleSidebar = () => {
-    setSidebarOpen(!sidebarOpen);
-  };
 
   return (
     <>
       <section className="navbar">
+        {/* ===== TOP BAR ===== */}
         <div className="navbar-top">
           <div className="navbar-container">
             <div className="navbar-top-left">
-              <span><img className="phone-icon" src={phoneIcon} alt="" /> +92 300 123 4567</span>
-              <span><img className="email-icon" src={emailIcon} alt="" /> info@bossleathers.com</span>
+              <span><img src={phoneIcon} alt="" /> +92 300 123 4567</span>
+              <span><img src={emailIcon} alt="" /> info@bossleathers.com</span>
             </div>
 
             <div className="navbar-top-right">
               <button onClick={() => setIsSearchOpen(true)}>
                 <span className="search-icon"><FiSearch /></span>
-                Search
-              </button>
+                Search</button>
             </div>
           </div>
         </div>
 
+        {/* ===== MAIN BAR ===== */}
         <div className="navbar-main">
           <div className="navbar-container">
-            <div className="navbar-menu">
-              <img 
-                src={menu} 
-                alt="" 
-                onClick={toggleSidebar} 
-                style={{ cursor: "pointer" }} 
-              />
-            </div>
-
-            <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
+            <div className="navbar-menu"><img src={menu} alt="" /></div>
 
             <span className="navbar-logo">
-              <img src="/logo2.svg" alt="Boss Leathers" />
+              <img src={bossLogo} alt="Boss Leathers" />
             </span>
 
             <div className="navbar-cart">
-              <img src={cart} alt="" onClick={() => navigate("/empty-cart")}/>
+              <img src={cart} alt="" />
               <span className="cart-count">0</span>
             </div>
           </div>
         </div>
       </section>
 
+      {/* Search Overlay - Conditionally Rendered */}
       {isSearchOpen && <SearchOverlay onClose={() => setIsSearchOpen(false)} />}
     </>
   );
